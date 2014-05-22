a09c8520cdc6176297b1e332aa43d2cf65f64904
NotTheEvilOne <git-NTEO@vplace.de>
2014-05-22 20:12:02 +0200

    Enhancement: Splitted HTTP client to return raw dictionaries or an response object Enhancement: Added reader mixin to support chunked encoded streams Change: Removed method name grouping from all source code files Trivial: Changed formatting of hanging indent to be PEP 8 conform

---
+++ src/dNG/data/http/response.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+RFC compliant and simple HTTP client
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?py;rfc_http_client
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(rfcHttpClientVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+class Response(object):
+#
+	"""
+HTTP response object handling chunked transfer-encoded data transparently.
+
+:author:    direct Netware Group
+:copyright: (C) direct Netware Group - All rights reserved
+:package:   rfc_http_client.py
+:since:     v0.1.01
+:license:   http://www.direct-netware.de/redirect.py?licenses;mpl2
+            Mozilla Public License, v. 2.0
+	"""
+
+	def __init__(self):
+	#
+		"""
+Constructor __init__(Response)
+
+:since: v0.1.01
+		"""
+
+		self.body_reader = None
+		"""
+Body reader callable
+		"""
+		self.exception = None
+		"""
+Exception occurred while receiving an response.
+		"""
+		self.headers = { }
+		"""
+Response headers
+		"""
+	#
+
+	def get_error_message(self):
+	#
+		"""
+Returns the error message based on the exception that occurred while
+processing the request.
+
+:return: (str) Error message; None otherwise
+:since:  v0.1.01
+		"""
+
+		return (None if (self.exception == None) else str(self.exception))
+	#
+
+	def get_exception(self):
+	#
+		"""
+Returns the exception that occurred while processing the request.
+
+:return: (object) Exception instance; None otherwise
+:since:  v0.1.01
+		"""
+
+		return self.exception
+	#
+
+	def get_header(self, name):
+	#
+		"""
+Returns the response header if defined.
+
+:param name: Header name
+
+:return: (str) Header value if set; None otherwise
+:since:  v0.1.01
+		"""
+
+		name = name.lower().replace("-", "_")
+		return self.headers.get(name)
+	#
+
+	def is_readable(self):
+	#
+		"""
+Returns true if the body reader is ready to receive the response and no
+exception occurred while processing the request.
+
+:return: (bool) True if ready
+:since:  v0.1.01
+		"""
+
+		return (self.body_reader != None and self.exception == None)
+	#
+
+	def read(self, size = -1):
+	#
+		"""
+Reads data using the given body reader. Chunked transfer-encoded data is
+handled automatically.
+
+:param size: Bytes to read
+
+:return: (bytes) Data received
+
+		"""
+
+		return (self.body_reader() if (size < 1) else self.body_reader(size))
+	#
+
+	def _set_body_reader(self, body_reader):
+	#
+		"""
+Sets the body reader callable of this response object.
+
+:param body_reader: Body reader callable
+
+:since: v0.1.01
+		"""
+
+		self.body_reader = body_reader
+	#
+
+	def _set_exception(self, exception):
+	#
+		"""
+Sets the exception occurred while processing the request.
+
+:param exception: Exception object
+
+:since: v0.1.01
+		"""
+
+		self.exception = exception
+	#
+
+	def _set_headers(self, headers):
+	#
+		"""
+Sets the headers of the response.
+
+:param headers: HTTP response headers
+
+:since: v0.1.01
+		"""
+
+		self.headers = headers
+	#
+#
+
+##j## EOF
---
+++ src/dNG/net/http/chunked_reader_mixin.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+RFC compliant and simple HTTP client
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?py;rfc_http_client
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(rfcHttpClientVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+# pylint: disable=invalid-name
+
+from time import time
+
+try:
+#
+	_PY_BYTES = unicode.encode
+	_PY_BYTES_DECL = str
+#
+except NameError:
+#
+	_PY_BYTES = str.encode
+	_PY_BYTES_DECL = lambda x: bytes(x, "raw_unicode_escape")
+#
+
+class ChunkedReaderMixin(object):
+#
+	"""
+HTTP reader handling chunked transfer-encoded data.
+
+:author:    direct Netware Group
+:copyright: (C) direct Netware Group - All rights reserved
+:package:   rfc_http_client.py
+:since:     v0.1.00
+:license:   http://www.direct-netware.de/redirect.py?licenses;mpl2
+            Mozilla Public License, v. 2.0
+	"""
+
+	BINARY_NEWLINE = _PY_BYTES_DECL("\r\n")
+	"""
+Newline bytes used in raw HTTP data
+	"""
+
+	def __init__(self):
+	#
+		"""
+Constructor __init__(ChunkedReaderMixin)
+
+:since: v0.1.00
+		"""
+
+		self.chunked_reader_buffer = None
+		"""
+Input pointer
+		"""
+	#
+
+	def _read_chunked_data(self, reader, callback, size = -1, timeout = None):
+	#
+		"""
+Reads chunked data from the given reader to the given callback.
+
+:param reader: Read callback
+:param callback: Callback for data read
+:param size: Byte size to read
+:param timeout: Timeout in seconds
+
+:since: v0.1.00
+		"""
+
+		is_last_chunk = False
+		size_read = 0
+		size_unread = 5
+		timeout_time = (-1 if (timeout == None) else time() + timeout)
+
+		chunk_buffer = self.chunked_reader_buffer
+		chunk_size = len(chunk_buffer)
+
+		self.chunked_reader_buffer = None
+
+		while (size_unread > 0
+		       and (size_read < size or chunk_size > 0)
+		       and (timeout_time < 0 or time() < timeout_time)
+		      ):
+		#
+			"""
+Read remaining data from last chunk
+			"""
+
+			part_size = (4096 if (size_unread > 4096) else size_unread)
+			part_data = reader(part_size)
+			part_size = len(part_data)
+
+			if (part_size < 1): raise OSError("Reader pointer could not be read before timeout occurred")
+			size_unread -= part_size
+
+			"""
+Get size for next chunk
+			"""
+
+			if (chunk_size < 1):
+			#
+				newline_position = (part_data
+				                    if (chunk_buffer == None) else
+				                    chunk_buffer + part_data
+				                   ).find(ChunkedReaderMixin.BINARY_NEWLINE)
+
+				chunk_octets = None
+
+				if (newline_position < 0):
+				#
+					if (chunk_buffer == None): chunk_buffer = part_data
+					else: chunk_buffer += part_data
+
+					part_size = 0
+					size_unread += 3
+				#
+				elif (chunk_buffer != None):
+				#
+					chunk_octets = (chunk_buffer + part_data)[:newline_position]
+					part_data = (chunk_buffer + part_data)[2 + newline_position:]
+					part_size = len(part_data)
+
+					chunk_buffer = None
+				#
+				elif (not is_last_chunk):
+				#
+					chunk_octets = part_data[:newline_position]
+					part_data = part_data[2 + newline_position:]
+
+					part_size = len(part_data)
+				#
+				else: part_size = 0
+
+				if (chunk_octets != None):
+				#
+					chunk_size = int(chunk_octets, 16)
+
+					if (chunk_size == 0): is_last_chunk = True
+					else: size_unread += chunk_size
+				#
+			#
+
+			if (part_size > 0):
+			#
+				chunk_size -= part_size
+
+				if (size_read > size):
+				#
+					if (self.chunked_reader_buffer == None): self.chunked_reader_buffer = part_data
+					else: self.chunked_reader_buffer += part_data
+				#
+				elif ((size_read + part_size) > size):
+				#
+					size_expected = size - (size_read + part_size)
+
+					self.chunked_reader_buffer = part_data[size_expected:]
+					callback(part_data[:size_expected])
+				#
+				else: callback(part_data)
+
+				size_read += part_size
+			#
+		#
+	#
+
+	def _reset_chunked_buffer(self):
+	#
+		"""
+Resets the buffer of chunked data remaining after the last read call.
+
+:since: v0.1.00
+		"""
+
+		self.chunked_reader_buffer = None
+	#
+#
+
+##j## EOF
