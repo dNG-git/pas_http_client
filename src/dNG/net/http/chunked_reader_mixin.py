c71bdfa7e9a5cb9596a9f44d28310d3556631266
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:40:03 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/net/http/chunked_reader_mixin.py
+++ src/dNG/net/http/chunked_reader_mixin.py
@@ -6,13 +6,13 @@ RFC compliant and simple HTTP client
 An abstracted programming interface for an HTTP client
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?py;rfc_http_client
+https://www.direct-netware.de/redirect?py;rfc_http_client
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(rfcHttpClientVersion)#
 #echo(__FILEPATH__)#
@@ -42,7 +42,7 @@ HTTP reader handling chunked transfer-encoded data.
 :copyright: (C) direct Netware Group - All rights reserved
 :package:   rfc_http_client.py
 :since:     v0.1.00
-:license:   http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:   https://www.direct-netware.de/redirect?licenses;mpl2
             Mozilla Public License, v. 2.0
 	"""
 
