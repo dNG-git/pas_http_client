3003da4b8d7e33926ab42efcab08d9b5fdcc89ec
NotTheEvilOne <git-NTEO@vplace.de>
2019-05-16 08:51:11 +0200

    Remove whitespace in "setup.py"
    (Trivial)

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ with TemporaryDirectory(dir = ".") as build_directory:
                "package_dir": { "": package_dir },
                "packages": [ "pas_http_client" ],
                "data_files": [ ( "docs", [ "LICENSE", "README" ]) ],
-               "test_suite" : "tests"
+               "test_suite": "tests"
              }
 
     # Override build_py to first run builder.py
